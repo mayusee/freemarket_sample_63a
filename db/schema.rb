8d66b1d7b1559bb29ffe0b64430832300c6d93c1
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-02-24 13:23:10 +0900

    add column status_num to items
    To move the columns of the trades table to the items table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_103657) do
+ActiveRecord::Schema.define(version: 2020_02_24_034629) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_02_15_103657) do
     t.datetime "sold_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status_num", limit: 1, null: false, unsigned: true
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["condition_num"], name: "index_items_on_condition_num"
