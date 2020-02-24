e3f745c379d7106d98b20769958367220e292beb
tomoyaueno29 <freemarket.sample.63a@gmail.com>
2020-02-24 16:10:12 +0900

    create_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,23 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      
+      image_params[:images].each do |image|
+        @item.images.build
+        item_image = @item.images.new(image: image)
+        item_image.save
+      end
+
+      respond_to do |format|
+        format.json
+      end
+    end
+  end
+
   def show
   end
   
