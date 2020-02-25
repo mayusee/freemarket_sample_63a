4b48f108094415eebdac893c3dfd2e929e04b92b
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-02-26 01:03:37 +0900

    fixed seeds.rb
    Modified to execute only development for user data and item data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
+require './db/seeds/area.rb'
+require './db/seeds/category.rb'
+require './db/seeds/brand.rb'
+require './db/seeds/shippingway.rb'
 if Rails.env == "development"
-  # require './db/seeds/area.rb'
-  # require './db/seeds/user.rb'
-  # require './db/seeds/category.rb'
-  # require './db/seeds/brand.rb'
+  require './db/seeds/user.rb'
   require './db/seeds/item.rb'
 end
