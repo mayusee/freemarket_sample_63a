26f13209d76c306993147684dc0f38ca34189bed
tomoyaueno29 <freemarket.sample.63a@gmail.com>
2020-02-26 21:48:48 +0900

    item_drag&drop

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to    :brand
   belongs_to    :category
   belongs_to    :shippingway
-
+  
   belongs_to    :user
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for      :item_images, allow_destroy: true
