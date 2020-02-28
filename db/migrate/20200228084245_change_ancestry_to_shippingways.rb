f8b1975394186f092d73d6fb0fc5229be3e64931
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-02-28 20:03:38 +0900

    fix shippingway references
    Change reference type to null permission

--- db/migrate/20200228084245_change_ancestry_to_shippingways.rb
+++ db/migrate/20200228084245_change_ancestry_to_shippingways.rb
@@ -1,6 +1,7 @@
 class ChangeAncestryToShippingways < ActiveRecord::Migration[5.2]
   def change
-    change_column :shippingways, :ancestry , :string
-    add_index :shippingways, :ancestry    
+    remove_column :shippingways, :ancestry , :string
+    add_column :shippingways, :ancestry , :string, null: true
+    add_index :shippingways, :ancestry
   end
 end
