cad6f578d8e36e2558c34f84f15c6353b71712af
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-02-29 21:16:13 +0900

    remove association shipings

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,13 @@ class Item < ApplicationRecord
   # associations
   belongs_to    :brand
   belongs_to    :category
+  belongs_to    :product_size
   belongs_to    :shippingway, optional: true
   belongs_to    :buyer, class_name: "User", optional: true
+  belongs_to    :buyer_address, class_name: "Address", optional: true
   belongs_to    :seller, class_name: "User"
+  belongs_to    :seller_address, class_name: "Address"
 
   has_many      :item_images
-
-  has_one       :shipping
 
 end
