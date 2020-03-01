8c1972332071e152442a1c32b05e1863daf4e57d
tomoyaueno29 <freemarket.sample.63a@gmail.com>
2020-03-01 11:28:11 +0900

    exibition_markup

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,10 @@
 module ItemsHelper
+  def text_placeholder
+    <<-"EOS".strip_heredoc
+      商品の説明(必須1000文字以内)
+      (色、素材、重さ、定価、注意点など)
+      例)2010年頃に1万円で購入したジャケットです。ライトグレーで傷はありませ
+      ん。あわせやすいのでおすすめです。
+    EOS
+  end
 end
