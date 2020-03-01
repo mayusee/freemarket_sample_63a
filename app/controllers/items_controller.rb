a3d4bb692493184fd7dc9cf4d1f9a85bb803d0e4
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-02 01:22:18 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :set_user_address,only: :create
+
   def index
   end
   
   def new
-    @item = Item.new
+    @item = Item.new(feerate: 0.1)
     5.times {@item.item_images.build}
   end
 
   def create
     @item = Item.new(item_params)
-
+    @item["seller_id"] = current_user.id
+    @item["seller_address_id"] = @address.id
+    @item["feerate"] = 0.1
+    @item["profit_price"] = @item.price - (@item.price * @item.feerate)
     if @item.save
       
-      image_params[:images].each do |image|
-        @item.images.build
-        item_image = @item.images.new(image: image)
-        item_image.save
-      end
+      # image_params[:images].each do |image|
+      #   @item.images.build
+      #   item_image = @item.images.new(image: image)
+      #   item_image.save
+      # end
 
-      respond_to do |format|
-        format.html { redirect_to root_path}
-        format.json
-      end
-
+      # respond_to do |format|
+      #   format.html { redirect_to root_path}
+      #   format.json
+      # end
       redirect_to root_path
     end
+
   end
 
   def show
@@ -32,11 +37,15 @@ class ItemsController < ApplicationController
 
   private
     def item_params
-      params.require(:item).permit(:brand_id,:category_id,:shippingway_id,:size_num,:condition_num,:shippingcharge_num,:daystoship_num,:title,:description,:price,:feerate,:profit_price, item_image_attributes: [:image])
+      params.require(:item).permit(:brand_id,:category_id,:shippingway_id,:size_num,:condition_num,:daystoship_num,:title,:description,:price)
     end
     
     def image_params
       params.require(:item_image).permit({:image => []})
     end
 
+    def set_user_address
+      #user_id:current_user.id
+      @address = Address.find_by(user_id: 1,status_num: 0)
+    end
 end
