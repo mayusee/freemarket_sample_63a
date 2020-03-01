4a30ff4c9e0c012146910601d246d2788d36891d
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-01 16:14:50 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,10 @@ require './db/seeds/area.rb'
 require './db/seeds/category.rb'
 require './db/seeds/brand.rb'
 require './db/seeds/shippingway.rb'
-# ↓ まだ作成中のためコメントアウト。
-# require './db/seeds/product_size.rb'
-# require './db/seeds/category_size.rb'
+require './db/seeds/product_size.rb'
+require './db/seeds/category_size.rb'
 if Rails.env == "development"
   require './db/seeds/user.rb'
-  # ↓ product_size, category_sizeのシードができるまでコメントアウト。
-  # require './db/seeds/item.rb'
+  require './db/seeds/address.rb'
+  require './db/seeds/item.rb'
 end
