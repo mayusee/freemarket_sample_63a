235b6cc93fbe9160c46df0e9a675a9d24ed1626c
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-01 12:39:47 +0900

    refactored indents
    SSIA

--- db/migrate/20200215082321_add_ancestry_to_product_sizes.rb
+++ db/migrate/20200215082321_add_ancestry_to_product_sizes.rb
@@ -2,6 +2,6 @@
 class AddAncestryToProductSizes < ActiveRecord::Migration[5.2]
   def change
     add_column :product_sizes, :ancestry, :string, null: true
-    add_index :product_sizes, :ancestry
+    add_index  :product_sizes, :ancestry
   end
 end
--- db/migrate/20200215082333_create_category_sizes.rb
+++ db/migrate/20200215082333_create_category_sizes.rb
@@ -3,7 +3,7 @@ class CreateCategorySizes < ActiveRecord::Migration[5.2]
     create_table :category_sizes do |t|
       # references
       t.references :category,           null: false, foreign_key: true
-      t.references :product_size,      null: false, foreign_key: true
+      t.references :product_size,       null: false, foreign_key: true
 
       t.timestamps
     end
