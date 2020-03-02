334d97b9600906b73327a6964025a95102393ef7
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-02 20:03:53 +0900

    Update trades_controller.rb
    first commit

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,2 +1,16 @@
 class TradesController < ApplicationController
+  before_action :get_item, only: [:new]
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def get_item
+    @item = Item.find(params[:group_id])
+  end
+
 end
