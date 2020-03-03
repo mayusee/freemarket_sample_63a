db5ffbe7f4f58a59baff0813a5245701cd7af1a2
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-03 20:10:50 +0900

    added associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,11 @@ class Item < ApplicationRecord
   belongs_to    :category
   belongs_to    :product_size, optional: true
   belongs_to    :shippingway,  optional: true
+  belongs_to    :user
+  belongs_to    :address
+  has_many      :trades
   has_many      :item_images,  dependent: :destroy
   accepts_nested_attributes_for      :item_images, allow_destroy: true
-
-  belongs_to    :seller,         class_name: "User"
-  belongs_to    :seller_address, class_name: "Address"
-  belongs_to    :buyer,          class_name: "User",    optional: true
-  belongs_to    :buyer_address,  class_name: "Address", optional: true
 
   # enum
   enum condition_num:{ brand_new: 0, near_new: 1, no_dirt: 2, near_dirt:3 ,dirty:4, bad_condition:5 }
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,9 @@
 class Trade < ApplicationRecord
+  # associations
+  belongs_to    :item
+  belongs_to    :user
+  belongs_to    :address
+
+  # enum
+  enum status_num:{sold_out:0, under_delivery:1, finished:2 }
 end
