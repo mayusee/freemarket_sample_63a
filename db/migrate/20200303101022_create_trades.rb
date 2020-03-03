846b0def1b29f0d482f9391536334325947920ac
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-03 20:11:00 +0900

    Update 20200303101022_create_trades.rb

--- db/migrate/20200303101022_create_trades.rb
+++ db/migrate/20200303101022_create_trades.rb
@@ -1,7 +1,13 @@
 class CreateTrades < ActiveRecord::Migration[5.2]
   def change
     create_table :trades do |t|
+      # references
+      t.references :item,                 null: false, foreign_key: true
+      t.references :user,                 null: false, foreign_key: true
+      t.references :address,              null: false, foreign_key: true
 
+      # enum
+      t.integer    :status_num,           null: false, limit: 1, unsigned: true, default: 0
       t.timestamps
     end
   end
