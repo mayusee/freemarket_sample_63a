798ac966a2daab665c72e4a96d86642a40e9cc64
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-07 06:09:19 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,7 +2,7 @@ class TopsController < ApplicationController
   def index
     @brands = Brand.order("RAND()").limit(1)
     id = @brands.ids
-    @items = Item.includes(:user).order('items.created_at desc').limit(3)
-    @items_brands = Item.includes(:user).where(brand_id: id).order('items.created_at desc').limit(3)
+    @items = Item.order('items.created_at desc').limit(3)
+    @items_brands = Item.where(brand_id: id).order('items.created_at desc').limit(3)
   end
 end
