cb24fc67c9839f166d471bdf18bd91c9b58dc645
mayusee <57439573+mayusee@users.noreply.github.com>
2020-03-08 17:22:38 +0900

    Install Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,11 +12,11 @@ set :repo_url,  'git@github.com:freemarket-sample-63a/freemarket_sample_63a.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/finalstage_a.pem'] 
+                  keys: ['~/.ssh/finalstage.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
