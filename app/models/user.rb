38410fa532fd834a9311827adf78f581281d766c
miura39 <miurast.miura.y@gmail.com>
2020-03-08 19:05:56 +0900

    error_message_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,22 @@ class User < ApplicationRecord
   has_many   :creditcards
   has_many   :addresses
   has_many   :trades
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d!@#\$%\^\&*\)\(+=._-]{7,255}\z/i
+  VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_POSTAL_CODE = /\A\d{3}-?\d{4}\z/
+
+  validates :nickname, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'は有効でありません。' }
+  validates :password, presence: true, length: { in: 7..20 }, format: { with: VALID_PASSWORD_REGEX, message: 'は20文字以下に設定して下さい'}
+  validates :password_confirmation, presence: true, length: { in:7..20}, format: {with:VALID_PASSWORD_REGEX, message: 'は20文字以下に設定して下さい'}
+  validates :birthday, presence: true
+  validates :last_name, presence: true, length: { maximum: 15 }
+  validates :first_name, presence: true, length: { maximum: 15 }
+  validates :last_name_kana, presence: true, length: { maximum: 20 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
+  validates :first_name_kana, presence: true, length: { maximum: 20 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい。'}
+  validates :telephone_number, presence: true, format: { with: VALID_PHONE_REGEX, message: 'は有効でありません。'}
+
 end
