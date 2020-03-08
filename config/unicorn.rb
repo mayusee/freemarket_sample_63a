5bcb19b40a755f4f8c311e104593c89f9d59ef8c
mayusee <57439573+mayusee@users.noreply.github.com>
2020-03-08 15:59:26 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,13 +8,13 @@ worker_processes 1
 working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}tmp/pids/unicorn.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen 3000
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}log/unicorn.stderr.log"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/log/unicorn.stdout.log"
@@ -52,4 +52,4 @@ end
 
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
-end 
+end
