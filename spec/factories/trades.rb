2e862c412b2ebef9841c3601844397192cd47111
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-08 21:59:08 +0900

    update factory bot to trade
    SSIA

--- spec/factories/trades.rb
+++ spec/factories/trades.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :trade do
-    
+    item_id               {11}
+    user_id               {2}
+    address_id            {2}
+    status_num            {0}
   end
 end
