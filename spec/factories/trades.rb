939239d87b1c792470052e605ba3a8f3fd4ff101
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-11 19:21:46 +0900

    Update trades.rb

--- spec/factories/trades.rb
+++ spec/factories/trades.rb
@@ -2,10 +2,11 @@ FactoryBot.define do
 
   factory :trade do
 
-    item_id               {1}
-    user_id               {1}
-    address_id            {1}
-    status_num            {0}
+    status_num         {0}
+
+    association :item
+    association :user
+    association :address
 
   end
 
