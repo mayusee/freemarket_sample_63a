c515c3ce752bd287f0b0bf0372ae5189db657df1
yukitaka0915-se <kato.engineering.09.15@gmail.com>
2020-03-12 20:53:29 +0900

    Update trade_spec.rb
    ・"Describe '#create' do" is unnecessary because it is not needed for model testing.
    ・If association is set up with factory_bot, valid? will be true even if the foreign key is nil.
    Therefore, executing expect (trade.errors [: item_id]). To is not described in RSpec because executing itself would result in an error.

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,32 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
-  describe '#create' do
-    # Factory_botでassociationを組むと外部キーの未入力テストが失敗するので、
-    # 解決するまでコメントアウトする。
-    # it "is invalid without a item_id" do
-    #   trade = build(:trade)
-    #   trade.valid?
-    #   expect(trade.errors[:item_id]).to include("を入力してください")
-    # end
-    # it "is invalid without a user_id" do
-    #   trade = build(:trade, user_id: nil)
-    #   trade.valid?
-    #   expect(trade.errors[:user_id]).to include("を入力してください")
-    # end
-    # it "is invalid without a address_id" do
-    #   trade = build(:trade, address_id: nil)
-    #   trade.valid?
-    #   expect(trade.errors[:address_id]).to include("を入力してください")
-    # end
-    it "is invalid without a status_num" do
-      trade = build(:trade, status_num: nil)
-      trade.valid?
-      expect(trade.errors[:status_num]).to include("を入力してください")
-    end
-    it "is valid trade" do
-      trade = build(:trade)
-      expect(trade).to be_valid
-    end
+  it "is valid trade" do
+    trade = build(:trade)
+    expect(trade).to be_valid
+  end
+  it "is invalid without a status_num" do
+    trade = build(:trade, status_num: nil)
+    trade.valid?
+    expect(trade.errors[:status_num]).to include("を入力してください")
   end
 end
