02567d3930c9d9ed62d10e9e27c18b8e42d2ece3
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-03-13 19:14:15 +0900

    Update address.rb

--- db/seeds/address.rb
+++ db/seeds/address.rb
@@ -1,16 +1,33 @@
-user_id = 1
-area_id = 13
-status_num = 0
-postal_number = "150-0041"
-city = "東京都渋谷区神南"
-number = "１丁目１２−１６"
-building = "アジアビル"
-telephone_number = "0312345678"
-Address.create!(user_id:  user_id,
-  area_id:          area_id,
-  status_num:       status_num,
-  postal_number:    postal_number,
-  city:             city,
-  number:           number,
-  building:         building,
-  telephone_number: telephone_number)
+ary_last_name = ["一","二"]
+ary_last_name_kana = ["イチ","ジ"]
+ary_area_id = [13,12]
+ary_postal_number = ["1500041","1500040"]
+ary_number = ["１丁目１２−１６","１丁目１８−２"]
+ary_building = ["アジアビル","フレーム神南坂"]
+
+2.times do |n|
+  user_id = n+1
+  first_name = "鈴木"
+  last_name = ary_last_name[n] + "郎"
+  first_name_kana = "スズキ"
+  last_name_kana = ary_last_name_kana[n] + "ロウ"
+  area_id = ary_area_id[n]
+  status_num = 0
+  postal_number = ary_postal_number[n]
+  city = "東京都渋谷区神南"
+  number = ary_number[n]
+  building = ary_building[n]
+  telephone_number = "0312345678"
+  Address.create!(user_id:          user_id,
+    area_id:          area_id,
+    first_name:       first_name,
+    last_name:        last_name,
+    first_name_kana:  first_name_kana,
+    last_name_kana:   last_name_kana,
+    status_num:       status_num,
+    postal_number:    postal_number,
+    city:             city,
+    number:           number,
+    building:         building,
+    telephone_number: telephone_number)
+end
